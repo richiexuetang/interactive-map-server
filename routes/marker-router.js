4f88005832f94bab2cc1f1f0c7303f28525bbcd9
richiexuetang <richard772794659@gmail.com>
2023-06-11 00:35:33 -0700

    less strict type check

--- routes/marker-router.js
+++ routes/marker-router.js
@@ -4,22 +4,39 @@ const router = express.Router();
 const db = require("../db");
 
 router.get("/markers/area/:mapSlug", async (req, res) => {
-    const {mapSlug} = req.params;
-    let collection = await db.collection("markers");
-    let results = await collection.find({mapSlug: mapSlug})
-      .toArray();
-  
-    res.send(results).status(200);
-  });
+  const { mapSlug } = req.params;
+  let collection = await db.collection("markers");
+  let results = await collection
+    .find({ mapSlug: mapSlug })
+    .sort({ lat: -1 })
+    .toArray();
+
+  res.send(results).status(200);
+});
 
 router.get("/markers/:mapSlug/:markerTypeId", async (req, res) => {
-    const {mapSlug, markerTypeId} = req.params;
-    console.log(mapSlug, markerTypeId)
-    let collection = await db.collection("markers");
-    let results = await collection.find({mapSlug: mapSlug, markerTypeId: parseInt(markerTypeId)})
-      .toArray();
-  
-    res.send(results).status(200);
-  });
+  const { mapSlug, markerTypeId } = req.params;
+  let projection = {
+    _id: 1,
+    coordinate: 1,
+    categoryId: 1,
+    markerName: 1,
+    description: 1,
+  };
+  if (markerTypeId == 2) {
+    projection = { _id: 1, coordinate: 1, markerName: 1, zoomRange: 1 };
+  } else if (markerTypeId == 3) {
+    projection = { _id: 1, categoryId: 1, coordinate: 1 };
+  } else if (markerTypeId == 4) {
+    projection = { _id: 1, path: 1, parentId: 1 };
+  }
+
+  let collection = await db.collection("markers");
+  let results = await collection
+    .find({ mapSlug: mapSlug, markerTypeId: parseInt(markerTypeId) }).project(projection)
+    .toArray();
+
+  res.send(results).status(200);
+});
 
 module.exports = router;
