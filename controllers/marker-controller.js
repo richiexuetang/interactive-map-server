8f6cd66aa45ef4f28404ae83677a8c419c60031f
richiexuetang <richard772794659@gmail.com>
2023-06-10 23:33:20 -0700

    add endpoint, remove unused

--- controllers/marker-controller.js
+++ controllers/marker-controller.js
@@ -1,57 +1,9 @@
 const { trusted } = require("mongoose");
 const Marker = require("../models/marker-model");
 
-createMarker = (req, res) => {
-  const body = req.body;
 
-  if (!body) {
-    return res.status(400).json({
-      success: false,
-      error: "You must provide a marker",
-    });
-  }
-
-  const marker = new Marker(body);
-
-  if (!marker) {
-    return res.status(400).json({ success: false, error: err });
-  }
-
-  marker
-    .save()
-    .then(() => {
-      return res.status(201).json({
-        success: true,
-        id: marker._id,
-        marker: marker,
-        message: "Marker created!",
-      });
-    })
-    .catch((error) => {
-      return res.status(400).json({
-        error,
-        message: "Marker not created!",
-      });
-    });
-};
-
-getMarkers = async (_, res) => {
-  await Marker.find({})
-    .then((markers, err) => {
-      if (err) {
-        return res.status(400).json({ success: false, error: err });
-      }
-      if (!markers.length) {
-        return res
-          .status(404)
-          .json({ success: false, error: `Marker not found` });
-      }
-      return res.status(200).json({ success: true, data: markers });
-    })
-    .catch((err) => console.log(err));
-};
-
-getMarkersByArea = async (req, res) => {
+const getMarkersByArea = async (req, res) => {
+  console.log(req.params)
   await Marker.find({ mapSlug: req.params.mapSlug }).sort({lat: 1})
 
     .then((markers, err) => {
@@ -64,73 +16,20 @@ getMarkersByArea = async (req, res) => {
     .catch((err) => console.log(err));
 };
 
-getMarkerById = async (req, res) => {
-  await Marker.findOne({ _id: req.params.id })
-    .then((marker, err) => {
+const getMarkersByAreaAndType = async (req, res) => {
+  console.log(req.params, 'type')
+  await Marker.find({ mapSlug: req.params.mapSlug, markerTypeId: req.params.markerTypeId }).sort({lat: 1})
+
+    .then((markers, err) => {
       if (err) {
         return res.status(400).json({ success: false, error: err });
       }
 
-      return res.status(200).json({ success: true, marker: marker });
+      return res.status(200).json({ success: true, data: markers });
     })
     .catch((err) => console.log(err));
-};
-
-getMarkerDetails = async (req, res) => {
-  await Marker.findOne({ _id: req.params.id })
-    .then((marker, err) => {
-      if (err) {
-        return res.status(400).json({ success: false, error: err });
-      }
-
-      const { descriptions, type } = marker;
-
-      return res
-        .status(200)
-        .json({
-          success: true,
-          details: { descriptions: descriptions, type: type },
-        });
-    })
-    .catch((err) => console.log(err));
-};
-
-deleteMarker = async (req, res) => {
-  await Marker.findOneAndDelete({ _id: req.params.id })
-    .then((marker, err) => {
-      if (err) {
-        return res.status(400).json({ success: false, error: err });
-      }
-
-      if (!marker) {
-        return res
-          .status(404)
-          .json({ success: false, error: `Marker not found` });
-      }
-
-      return res.status(200).json({ success: true, data: marker });
-    })
-    .catch((err) => console.log(err));
-};
-
-const updateMarker = async (req, res) => {
-  try {
-    const updatedResult = await Marker.findByIdAndUpdate(
-      { _id: req.params.id },
-      req.body
-    );
-    return res.status(200).json({ success: true, data: updatedResult });
-  } catch (error) {
-    console.log(error);
-  }
 };
 
 module.exports = {
-  createMarker,
-  getMarkers,
   getMarkersByArea,
-  deleteMarker,
-  updateMarker,
-  getMarkerById,
-  getMarkerDetails,
 };
---
+++ crud.js
@@ -0,0 +1,95 @@
+const { MongoClient } = require("mongodb");
+const { ObjectId } = require("mongodb"); // or ObjectID
+var fs = require("fs");
+
+async function main() {
+  const uri =
+    "mongodb+srv://rxtang:Yiersansi@interactive-map.vvpcrjm.mongodb.net/ritcher-map?retryWrites=true&w=majority";
+
+  const data = [];
+
+  const client = new MongoClient(uri);
+
+  try {
+    // Connect to the MongoDB cluster
+    await client.connect();
+
+    // Make the appropriate DB calls
+    // await createMarkers(client, data);
+    const result = await findNullLatLng(client);
+    for (var i = 0; i < result.length; i++) {
+      await client.db("ritcher-map")
+        .collection("markers")
+        .updateOne(
+          { _id: result[i]._id },
+          { $set: { lat: result[i].coordinate[0], lng: result[i].coordinate[1]} }
+        );
+    }
+  } finally {
+    // Close the connection to the MongoDB cluster
+    await client.close();
+  }
+}
+
+main().catch(console.error);
+
+// Add functions that make DB calls here
+async function createMarkers(client, newMarkers) {
+  const result = await client
+    .db("ritcher-map")
+    .collection("markers")
+    .insertMany(newMarkers);
+  console.log(
+    `New listing created with the following id: ${result.insertedId}`
+  );
+}
+
+async function findNullLatLng(client) {
+  const result = await client
+    .db("ritcher-map")
+    .collection("markers")
+    .find({ lat: null, lng: null, markerTypeId: 1 })
+    .toArray();
+  console.log(result);
+  return result;
+}
+
+async function updateDescription(client, pair) {
+  await client
+    .db("ritcher-map")
+    .collection("markers")
+    .updateOne({ _id: pair[0] }, { $set: { description: pair[1] } });
+}
+
+async function changeField(client) {
+  const markers = await client
+    .db("ritcher-map")
+    .collection("markers")
+    .find({})
+    .toArray();
+  const result = [];
+  markers.map((marker) => {
+    const { descriptions, _id } = marker;
+    if (descriptions && descriptions.length) {
+      let description = "";
+      descriptions.map((desc) => {
+        description += desc;
+      });
+      const pair = [_id, description];
+      result.push(pair);
+    }
+  });
+
+  //   var file = fs.createWriteStream("array.txt");
+
+  //   file.on("error", function (err) {
+  //     /* error handling */
+  //   });
+  //   result.forEach(function (v) {
+  //     console.log(v[0])
+  //     const val = "['" + v[0] + "','" +  v[1] + "'],"
+  //     file.write(val);
+  //   });
+  //   file.end();
+  return result;
+}
